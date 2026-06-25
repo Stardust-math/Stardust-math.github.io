e2dc480ece36abd65e42b2458ec5f65533c4a19f
Jinghao Chen <stardust.math26@Gmail.com>
2026-06-25 23:37:11 +0800

    Add Friends Links

--- assets/js/Config/SocialFriendsConfig.js
+++ assets/js/Config/SocialFriendsConfig.js
@@ -23,12 +23,26 @@
     ].join('\n'),
 
     friends: [
-      {
+      /*{
         title: 'Joker Chen',
         desc: 'Ich muß fort, ich muß reisen, ich muß in die Freiheit.',
         siteurl: 'https://stardust-math.github.io',
         avatar: 'https://stardust-math.github.io/assets/images/favicon.png',
         tag: 'Blog'
+      },*/
+      {
+        title: 'Jingyi Zhang',
+        desc: '事不宿夜，业不逾时。',
+        siteurl: 'https://jingyizhang05.github.io',
+        avatar: 'https://jingyizhang05.github.io/favicon.ico',
+        tag: 'Academic'
+      },
+      {
+        title: 'Yuzhou Zhu',
+        desc: '神',
+        siteurl: 'https://yuzhou541.github.io',
+        avatar: 'https://yuzhou541.github.io/pics/avatar/favicon.JPG',
+        tag: 'Academic'
       }
     ]
   };
